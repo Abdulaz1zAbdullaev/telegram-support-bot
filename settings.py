e802c70cb3cfa0f1ab2144180f537b7d74746d54
Abdulaz1zAbdullaev <101507668+Abdulaz1zAbdullaev@users.noreply.github.com>
2022-03-13 18:30:15 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,6 +17,6 @@ if TELEGRAM_SUPPORT_CHAT_ID is None or not str(TELEGRAM_SUPPORT_CHAT_ID).lstrip(
 TELEGRAM_SUPPORT_CHAT_ID = int(TELEGRAM_SUPPORT_CHAT_ID)
 
 
-WELCOME_MESSAGE = os.getenv("WELCOME_MESSAGE", "👋")
+WELCOME_MESSAGE = os.getenv("WELCOME_MESSAGE", "Assalomu alaykum, operatorlarimiz siz bilan tez orada bog'lanishadi, iltimos chatni boshlash uchun ismingizni kiriting !")
 REPLY_TO_THIS_MESSAGE = os.getenv("REPLY_TO_THIS_MESSAGE", "REPLY_TO_THIS")
 WRONG_REPLY = os.getenv("WRONG_REPLY", "WRONG_REPLY")
